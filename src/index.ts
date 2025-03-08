4238b1e718b41b90d3c714a0e8072431c78d631c
Senthilprabu Ponnusamy <p.senthilprabu@gmail.com>
2025-03-08 01:51:55 -0500

    chore(husky): set up pre-commit and commit-msg hooks with linting

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
  * This is a simple function that logs "Hello, World!" to the console.
  */
 export function helloWorld(): void {
+  // eslint-disable-next-line no-console
   console.log("Hello, World!");
 }
